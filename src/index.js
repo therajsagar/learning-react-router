dd666ca19e32d40019682d149afaaa0bf3bb7b67
r4jsg <therajsagar@gmail.com>
2019-01-03 23:55:33 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const Home = () => <div><h1>Home</h1></div>;
-//const About = () => <div><h1>About</h1></div>;
 const About = () => <div><h1>About</h1></div>;
 const Users = (props) => {
   return (
@@ -12,7 +11,35 @@ const Users = (props) => {
     </div>
 )}
 
-
+const Info = ({ match }) => {
+  return (
+    <div>
+       <ul>
+        <li>
+          <Link to={match.url + "/phone"}>Phone</Link>
+        </li>
+        <li>
+          <Link to={match.url + "/email"}>Email</Link>
+        </li>
+        <li>
+          <Link to={match.url + "/address"}>Address</Link>
+        </li>
+      </ul>
+      <Route
+        exact path={match.url + "/phone"}
+        render={() => <div><h1>123 456 7890</h1></div>}
+      />
+      <Route
+        exact path={match.url + "/email"}
+        render={() => <div><h1>email@email.com</h1></div>}
+      />
+      <Route
+        exact path={match.url + "/address"}
+        render={() => <div><h1>123 Address</h1></div>}
+      />
+    </div>
+  );
+};
 
 const App = () => (
   <BrowserRouter>
@@ -27,10 +54,14 @@ const App = () => (
         <li>
           <Link to="/users">Users</Link>
         </li>
+        <li>
+          <Link to="/info">Info</Link>
+        </li>
       </ul>
       <hr />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/info" component={Info} />
       <Route path="/users/:id" component={Users} />
       <Route exact path="/users" render={()=> <div><h1>Users</h1></div>} />
     </div>

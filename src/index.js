65ee45ca464433796584b0801cced1d0ed5b5d64
r4jsg <therajsagar@gmail.com>
2019-01-04 17:08:58 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Link, NavLink, Switch, Redirect, Prompt } from "react-router-dom";
 
+
+
 const Home = () => <div><h1>Home</h1></div>;
 const About = () => <div><h1>About</h1></div>;
 const Users = (props) => {
@@ -41,39 +43,41 @@ const Info = ({ match }) => {
   );
 };
 
-const DefaultRoute = ()=> <div><h1>Incorrect URL</h1></div>
+const DefaultRoute = ()=> <div><h1>Invalid URL</h1></div>
 
 const App = () => (
   <BrowserRouter>
     <div>
       <ul>
         <li>
-          <NavLink to="/home" activeStyle={{color: 'green'}}><h1>Home</h1></NavLink>
+          <NavLink to="/home" activeStyle={{color: 'green'}}><h2>Home</h2></NavLink>
         </li>
         <li>
-          <NavLink to="/about" activeStyle = {{color:'darkorange'}}><h1>About</h1></NavLink>
+          <NavLink to="/about" activeStyle = {{color:'darkorange'}}><h2>About</h2></NavLink>
         </li> 
         <li>
-          <Link to="/users/all"><h1>Users</h1></Link>
+          <Link to="/users/all"><h2>Users</h2></Link>
         </li>
         <li>
-          <Link to="/info"><h1>Info</h1></Link>
+          <Link to="/info"><h2>Info</h2></Link>
         </li>
       </ul>
       <hr />
       <Switch>
       <Route exact path="/home" component={Home} />
       <Redirect exact path="/"  to='/home'/>
-      <Prompt when={true} message="Do U really want 2 leave?"/>
       <Route path="/about" component={About} />
       <Route path="/info" component={Info} />
       <Route  path="/users/all" render={()=> <div><h1>Users</h1></div>} />
       <Route exact path="/users/:id" component={Users} />
       <Route component={DefaultRoute}/>      
+      <Prompt when={true} message="Are you sure you want to leave?"/>   
       </Switch>
     </div>
   </BrowserRouter>
 );
+
+
 
 render(<App />, document.getElementById("root"));
 

055a54bb99482f7eef95daebbc4f58d4817e7b43
r4jsg <therajsagar@gmail.com>
2019-01-04 15:05:15 +0530

    Redirects added..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 
 const Home = () => <div><h1>Home</h1></div>;
 const About = () => <div><h1>About</h1></div>;
@@ -48,13 +48,13 @@ const App = () => (
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/home">Home</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
         </li> 
         <li>
-          <Link to="/users/all">Users All</Link>
+          <Link to="/users/all">Users</Link>
         </li>
         <li>
           <Link to="/info">Info</Link>
@@ -62,11 +62,12 @@ const App = () => (
       </ul>
       <hr />
       <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
+      <Redirect exact path="/"  to='/home'/>
       <Route path="/about" component={About} />
       <Route path="/info" component={Info} />
       <Route  path="/users/all" render={()=> <div><h1>Users</h1></div>} />
-      <Route  path="/users/:id" component={Users} />
+      <Route exact path="/users/:id" component={Users} />
       <Route component={DefaultRoute}/>
       </Switch>
     </div>

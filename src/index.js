d07a5bb9ec6f6ef4f3cdae175ebc03ad7a8be935
r4jsg <therajsagar@gmail.com>
2019-01-04 16:00:53 +0530

    prompt concept

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Link, NavLink, Switch, Redirect, Prompt } from "react-router-dom";
 
 const Home = () => <div><h1>Home</h1></div>;
 const About = () => <div><h1>About</h1></div>;
@@ -16,13 +16,13 @@ const Info = ({ match }) => {
     <div>
        <ul>
         <li>
-          <Link to={match.url + "/phone"}>Phone</Link>
+          <Link to={match.url + "/phone"}><h2>Phone</h2></Link>
         </li>
         <li>
-          <Link to={match.url + "/email"}>Email</Link>
+          <Link to={match.url + "/email"}><h2>Email</h2></Link>
         </li>
         <li>
-          <Link to={match.url + "/address"}>Address</Link>
+          <Link to={match.url + "/address"}><h2>Address</h2></Link>
         </li>
       </ul>
       <Route
@@ -48,27 +48,28 @@ const App = () => (
     <div>
       <ul>
         <li>
-          <Link to="/home">Home</Link>
+          <NavLink to="/home" activeStyle={{color: 'green'}}><h1>Home</h1></NavLink>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <NavLink to="/about" activeStyle = {{color:'darkorange'}}><h1>About</h1></NavLink>
         </li> 
         <li>
-          <Link to="/users/all">Users</Link>
+          <Link to="/users/all"><h1>Users</h1></Link>
         </li>
         <li>
-          <Link to="/info">Info</Link>
+          <Link to="/info"><h1>Info</h1></Link>
         </li>
       </ul>
       <hr />
       <Switch>
       <Route exact path="/home" component={Home} />
       <Redirect exact path="/"  to='/home'/>
+      <Prompt when={true} message="Do U really want 2 leave?"/>
       <Route path="/about" component={About} />
       <Route path="/info" component={Info} />
       <Route  path="/users/all" render={()=> <div><h1>Users</h1></div>} />
       <Route exact path="/users/:id" component={Users} />
-      <Route component={DefaultRoute}/>
+      <Route component={DefaultRoute}/>      
       </Switch>
     </div>
   </BrowserRouter>
